e99c3b18bda0dfd14c0a0ab31bbc4b020d5cc3f4
Bentley Bigelow <147126011+tribecon1@users.noreply.github.com>
2025-01-15 15:15:48 -0700

    added import statement for chain_actions

--- windowsDNCfinder.py
+++ windowsDNCfinder.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Define necessary functions
 def install(package):
